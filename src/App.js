2e8204338f26835e36f8ed2a35ab40d498922736
Dawid <dawid.drozdzynski.g4@gmail.com>
2022-06-11 11:30:01 +0200

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -1,32 +1,63 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-const JOKE_API = 'http://api.icndb.com/jokes/random?';
+const JOKE_API = 'http://api.icndb.com/jokes/random';
 const CAT_API = 'http://api.icndb.com/categories';
 
 function App() {
   const [joke, setJoke] = useState('');
   const [category, setCategory] = useState('" ",nerdy, explicit');
   const [person, setPerson] = useState('Chuck Norris');
-  const [categoryList, setCategoryList] = useState([]);
   const words = person.split(' ');
+  const [categoryList, setCategoryList] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [jokeList, setJokeList] = useState([]);
 
   const displayJoke = () => {
-    fetch(JOKE_API+'firstName='+words[0]+'&lastName='+words[1]+'&limitTo=['+category+']')
+    fetch(JOKE_API+'?firstName='+words[0]+'&lastName='+words[1]+'&limitTo=['+category+']')
      .then(res => res.json())
-     .then(data => setJoke(data.value.joke))
-  }
+     .then(data => setJoke('"'+data.value.joke+'"'))
+  };
 
   const getCategoryList = () => {
     fetch(CAT_API)
      .then(res => res.json())
      .then(data => setCategoryList(data.value))
+  };
+
+  const getJokeList = () => {
+    console.log(number);
+    fetch(JOKE_API+'/'+number)
+     .then(res => res.json())
+     .then(data => setJokeList(data.value))
+    console.log(jokeList.length);
+  };
+
+  const downloadTxtFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob(jokeList, {
+      type: "text/plain"
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = "Chuck_jokes.txt";
+    document.body.appendChild(element);
+    element.click();
+  };
+
+  const saveJokes = () => {
+    getJokeList();
+    for (let i = 0; i < jokeList.length; i++) {
+    jokeList[i] = jokeList[i].joke+'\n'
+    }
+    //console.log(number,jokeList);
+    //downloadTxtFile();
   }
 
   useEffect(() => {
     displayJoke();
     getCategoryList();
-  }, [])
+    getJokeList();
+  }, []);
 
   return (
     <div className="box">
@@ -38,7 +69,9 @@ function App() {
         <option key={category}>{category}</option>
       ))}
       </select><br></br>
-      <input type="text" placeholder='Impersonate Chuck Norris' onChange={text => setPerson(text.target.value)}></input>
+      <input type="text" placeholder='Impersonate Chuck Norris' onChange={text => setPerson(text.target.value)}></input><br></br>
+      <input type="number" onChange={number => setNumber(number.target.value)}></input>
+      <button onClick={saveJokes}>Save Jokes</button>
     </div>
   );
 }
